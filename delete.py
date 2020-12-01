8448bbdefde47b1f0a1fa77dcf0721a8b6dc7a8f
Ninita693 <67185573+Ninita693@users.noreply.github.com>
2020-12-02 00:35:58 +0530

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,17 @@
+#for delete operation
+#use syntax "delete(key_name)"
+
+def delete(key):
+    if key not in d:
+        print("error: given key does not exist in database. Please enter a valid key") #error message4
+    else:
+        b=d[key]
+        if b[2]!=0:
+            if time.time()<b[2]: #comparing the current time with expiry time
+                del d[key]
+                print("key is successfully deleted")
+            else:
+                print("error: time-to-live of",key,"has expired") #error message5
+        else:
+            del d[key]
+            print("key is successfully deleted")
